09b271c234f9ea59ac4ccdf6d0e0dccf2e6fb340
alvarovg <a.vegasg.2023@alumnos.urjc.es>
2026-04-06 14:58:44 +0200

    S4

---
+++ P2/S4/Ej-06.js
@@ -0,0 +1,13 @@
+
+console.log("Ejecutando js...")
+
+//-- Leer el párrafo identificado como test
+const test = document.getElementById('test')
+
+//-- Configurar el manejador para el evento de
+//-- pulsación de botón
+test.onclick = function () {
+  console.log("Click sobre el párrafo...")
+}
+
+
